d5c5b0d54469d7267296e754bd67a1a1e61bfb2d
Hossein Salari <hosein.salari81@gmail.com>
2024-10-22 22:04:27 +0330

    fix blog errors

--- src/blog/blog.controller.ts
+++ src/blog/blog.controller.ts
@@ -1,18 +1,29 @@
 import {
   Body,
   Controller,
+  Delete,
+  Get,
   HttpCode,
   HttpStatus,
+  Param,
   Post,
+  Put,
+  Query,
   UseInterceptors,
 } from "@nestjs/common";
-import { ApiConsumes, ApiTags } from "@nestjs/swagger";
+import { ApiConsumes, ApiParam, ApiQuery, ApiTags } from "@nestjs/swagger";
 import { BlogService } from "./blog.service";
 import { CreateBlogDto } from "./dto/create.dto";
 import { MulterFile } from "src/common/types/public";
-import { CheckRequiredUploadedFile } from "src/common/decorator/upload-file.decorator";
+import {
+  CheckOptionalUploadedFile,
+  CheckRequiredUploadedFile,
+} from "src/common/decorator/upload-file.decorator";
 import { UploadFile } from "src/common/interceptor/upload-file.interceptor";
 import { MIME_TYPE } from "src/common/enum/meme-type.enum";
+import { SwaggerConsumes } from "src/common/enum/swagger.enum";
+import { UpdateBlogDto } from "./dto/update.dto";
+import { Blog } from "./schema/blog.schema";
 
 @ApiTags("Blog")
 @Controller("/blog")
@@ -32,5 +43,39 @@ export class BlogController {
       statusCode: HttpStatus.CREATED,
       message: "created blog successfully",
     };
+  }
+
+  @Get()
+  @ApiQuery({ name: "search", type: "string", required: false })
+  @ApiQuery({ name: "category", type: "string", required: false })
+  async findAll(
+    @Query("search") search: string,
+    @Query("category") category: string
+  ): Promise<{ blogs: Blog[] }> {
+    const blogs = await this.blogService.findAll({ search, category });
+    return { blogs };
+  }
+
+  @Get(":id")
+  @ApiParam({ name: "id", type: "string" })
+  async findOne(@Param("id") id: string): Promise<Blog> {
+    return await this.blogService.findOne(id);
+  }
+
+  @Put(":id")
+  @ApiConsumes(SwaggerConsumes.MULTIPART)
+  @ApiParam({ name: "id", type: "string" })
+  async update(
+    @CheckOptionalUploadedFile(MIME_TYPE.IMAGE) file: MulterFile,
+    @Param("id") id: string,
+    @Body() blog: UpdateBlogDto
+  ): Promise<Blog> {
+    return await this.blogService.update(id, blog);
+  }
+
+  @Delete(":id")
+  @ApiParam({ name: "id", type: "string" })
+  async delete(@Param("id") id: string): Promise<any> {
+    return await this.blogService.delete(id);
   }
 }
